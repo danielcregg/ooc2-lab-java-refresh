e3d7e888406bc938dfe8d1aa22113202e350e54b
Daniel Cregg <danielcregg@users.noreply.github.com>
2021-09-27 09:51:26 +0000

    Add while loop demo

---
+++ WhileLoopDemo.java
@@ -0,0 +1,11 @@
+public class WhileLoopDemo {
+    public static void main(String[] args) {
+        
+        int x = 1;
+
+        while(x <= 4){
+            // print the value of x here
+            // increment the value of x here
+        }
+    }
+}
