d5f17a03a04ea3de2624c0a6d549d7c6f5211906
Daniel Cregg <danielcregg@users.noreply.github.com>
2021-09-28 13:44:54 +0100

    Fix infinite loop

--- WhileLoopDemo.java
+++ WhileLoopDemo.java
@@ -2,10 +2,12 @@ public class WhileLoopDemo {
     public static void main(String[] args) {
         
         int x = 1;
-
-        while(x <= 4){
+    	
+        // Uncomment the while loop and add the two required lines.
+        
+        //while(x <= 4){
             // print the value of x here
             // increment the value of x here
-        }
+        //}
     }
 }
