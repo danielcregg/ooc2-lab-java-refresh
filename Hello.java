6ce2b2b1a837ae07f658aa5e3f85a904695d0377
Daniel Cregg <danielcregg@users.noreply.github.com>
2021-09-28 10:38:23 +0000

    Fixed hello world

--- Hello.java
+++ Hello.java
@@ -1,4 +1,5 @@
 public class Hello {
-    // Write the main method here
-    // Print "Hello World!" to the console
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
