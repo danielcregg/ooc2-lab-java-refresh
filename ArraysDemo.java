4a303664faad7233b1a5596f99f4d3838633b78d
Daniel Cregg Gitpod <daniel.cregg@atu.ie>
2022-09-23 10:27:37 +0000

    Update array demo

--- ArraysDemo.java
+++ ArraysDemo.java
@@ -1,20 +1,30 @@
+import java.util.concurrent.locks.Condition;
+
 public class ArraysDemo {
     
     public static void main(String[] args) {
         
-        String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        // Create a String array called daysOfWeek to hold the days of the week
+        // PUT YOUR CODE HERE
 
+        // Print the fouth day of the week
+        // PUT YOUR CODE HERE
+        
         System.out.println("The days of the week are: ");
-        for (int i = 0; i < daysOfWeek.length; i++) {
-            System.out.println(daysOfWeek[i]);
-        }
+        // PUT YOUR CODE HERE   
 
         System.out.println("The days of the week in reverse are: ");
-        for (int i = daysOfWeek.length - 1; i >= 0; i--) {
-            System.out.println(daysOfWeek[i]);
-        }
+        // PUT YOUR CODE HERE
 
-       // Print every other day of the week
+       // Print every second day of the week
+       System.out.println("Every second day of the week is: ");
+       for (int i = 0; i < daysOfWeek.length; i++) {
+           // Use and if statment combiuned with the modulous operator to print every second day 
+            //if (condition){
+            //    System.out.println(daysOfWeek[i]);
+            //} // End of if
 
-    }
-}
+        } // End of for loop
+
+    } // End of main
+} // End of class
