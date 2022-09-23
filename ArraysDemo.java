a5c29ce089bf49bd8e1e912fb9a96cf227f678aa
Daniel Cregg <danielcregg@users.noreply.github.com>
2022-09-23 10:11:16 +0000

    Update Arrays demo

--- ArraysDemo.java
+++ ArraysDemo.java
@@ -1,6 +1,20 @@
 public class ArraysDemo {
+    
     public static void main(String[] args) {
-        String[] daysOfWeek = {"Monday", ""};
+        
+        String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        System.out.println("The days of the week are: ");
+        for (int i = 0; i < daysOfWeek.length; i++) {
+            System.out.println(daysOfWeek[i]);
+        }
+
+        System.out.println("The days of the week in reverse are: ");
+        for (int i = daysOfWeek.length - 1; i >= 0; i--) {
+            System.out.println(daysOfWeek[i]);
+        }
+
+       // Print every other day of the week
 
     }
 }
