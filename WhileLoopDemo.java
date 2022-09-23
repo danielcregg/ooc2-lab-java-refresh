1537492312b23c77aefdf69ed83784b99f2d3252
Daniel Cregg Gitpod <daniel.cregg@atu.ie>
2022-09-23 09:51:14 +0000

    Add while loop

---
+++ WhileLoopDemo.java
@@ -0,0 +1,22 @@
+/**
+ * While loop demo program
+ */
+public class WhileLoopDemo {
+
+    public static void main(String[] args) {
+     
+        // Declare an integer and name it x. Initialise the value of x to 1. 
+        int x = 1;
+
+        // while loop executes until x <= 4
+        //while (condition) {
+            
+            // Print the value of x
+            // PUT YOUR CODE HERE
+
+            //Increment x for each iteration of the loop
+            // PUT YOUR CODE HERE
+
+        //}
+    }
+}
