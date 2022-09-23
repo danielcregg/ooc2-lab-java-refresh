3ed576b00afa6015504e8f35c5f9afdca0317c06
Daniel Cregg Gitpod <daniel.cregg@atu.ie>
2022-09-23 09:51:41 +0000

    Add If demo

---
+++ IfDemo.java
@@ -0,0 +1,27 @@
+// Import the Scanner class so that you take user input
+// YOUR CODE HERE
+
+public class IfDemo {
+    public static void main(String[] args) {
+        // Create a Scanner object that take input from the keyboard
+        // YOUR CODE HERE  
+        // Ask the user to input there age.
+        // YOUR CODE HERE
+        // Store the user input.
+        // YOUR CODE HERE
+        
+        //NOTE: The legal age to drive in Ireland is 17
+        // Check if the user is over 17
+        // if ( // YOUR CODE HERE){
+        //     System.out.println("You are old enough to drive a car!");
+        // } else {
+        //     System.out.println("You are not old enough to drive a car!");
+        // }
+
+        // Extend your if statement above to check if the user is over 16 and can drive a tractor.
+
+        // Close the Scanner object when you are finished using to save resources
+        // YOUR CODE HERE
+        
+    }
+}
