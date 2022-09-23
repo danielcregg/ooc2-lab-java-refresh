22699c60be5dd0ec2c53d10c423f8f55edcfa594
Daniel Cregg Gitpod <daniel.cregg@atu.ie>
2022-09-23 09:51:33 +0000

    Add for loop

---
+++ ForLoopDemo.java
@@ -0,0 +1,11 @@
+public class ForLoopDemo {
+    public static void main(String[] args) {
+        
+        //for(Declare and initialise x; condition; increment) {
+
+            // Print the value of x
+            // PUT YOUR CODE HERE
+
+        //}
+    }
+}
